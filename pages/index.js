1cae2318e4cd4eba6cbfcefad38529c84ea101e9
cisu <49948070+cisu@users.noreply.github.com>
2021-04-29 21:17:31 +0300

    add sass

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
 
 
 export default function HomePage() {
   return (
-    <div className={styles.container}>
+    <div>
+
+
       <h1>Home</h1>
     
     </div>
